f402f048acf504c52a584cdc47cd640e0a829197
jin.xia <karst.xia@msn.cn>
2018-03-10 21:53:12 +0800

    throw and catch api error

--- app/ctrls/user_ctrl.js
+++ app/ctrls/user_ctrl.js
@@ -1,12 +1,20 @@
+const ApiError = require('../error/ApiError')
+const api_code = require('../error/error_code')
+
+
 //获取用户
 exports.getUser = async (ctx, next) => {
+	var uid = ctx.query.uid || 0;
+	if (uid == 0) {
+		throw new ApiError(api_code.USER_NOT_EXIST);
+	}
     ctx.body = {
         username: 'karst.xia',
         age: 30
     }
-};
+}
 
 //用户注册
 exports.registerUser = async (ctx, next) => {
     console.log('registerUser', ctx.request.body);
-};
+}
