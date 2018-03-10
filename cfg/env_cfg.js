429f254ea1f07f7ddfabd3e63506e44faf7dd2ff
jin.xia <karst.xia@msn.cn>
2018-03-10 22:12:50 +0800

    adjust cfg

--- cfg/env_cfg.js
+++ cfg/env_cfg.js
@@ -1,5 +1,21 @@
-var env_dev = require('./env_dev');
-var env_test = require('./env_test');
+// 开发环境的配置内容
+const env_dev = {
+    env: 'DEVELOPEMENT',//环境名称
+    port: 8081,         //服务端口号
+    mongodb_url: '',    //数据库地址
+    redis_url:'',       //redis地址
+    redis_port: ''      //redis端口号
+};
+
+// 测试环境的配置内容
+const env_test = {
+    env: 'TEST',		//环境名称
+    port: 8082,         //服务端口号
+    mongodb_url: '',    //数据库地址
+    redis_url:'',       //redis地址
+    redis_port: ''      //redis端口号
+};
+
 
 //根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
 module.exports = {
