29b8f6aff8e9fc4c32a6c60c669d5239b878068a
karst.xia <karst.xia@msn.cn>
2018-03-12 11:07:45 +0800

    repo api from github

--- app/ctrl/repo_ctrl.js
+++ app/ctrl/repo_ctrl.js
@@ -2,15 +2,6 @@ const repo_model = require('../model/repo_model');
 
 
 exports.search = async (ctx, next) => {
-	// ctx.body 
-	const data = await repo_model.search(ctx.req.query);
-	var dataSet = repo_model.treeData(data);
-            // console.log(msg);
-            // return msg;
-            // ctx.res.json(dataSet);
-            ctx.body =  dataSet;
-    //         {
-    //     "name": "languages",
-    //     "children": []
-    // }
+	const result = await repo_model.search(ctx.req.query);
+	ctx.body = repo_model.format(result.data);
 }
--- app/model/repo_model.js
+++ app/model/repo_model.js
@@ -1,162 +1,73 @@
 const octokit = require('@octokit/rest')({
-    timeout: 8000, // 0 means no request timeout
+    timeout: 5000, // 0 means no request timeout
     requestMedia: 'application/vnd.github.v3+json',
     headers: {
-    'user-agent': 'octokit/rest.js v1.2.3' // v1.2.3 will be current version
+        'user-agent': 'octokit/rest.js v1.2.3' // v1.2.3 will be current version
     },
-
-    // change for custom GitHub Enterprise URL
-    host: 'api.github.com',
-    pathPrefix: '',
-    // optional 
-    debug: true,
-    protocol: 'https',
-    port: 443,
-
-    // Node only: advanced request options can be passed as http(s) agent
-    agent: undefined
+    agent: undefined // Node only: advanced request options can be passed as http(s) agent
 })
 
-// /**
-//  * from https://www.npmjs.com/package/github
-//  * @type {GitHubApi}
-//  */
-// var github = new GitHub({
-//     // required 
-//     version: "3.0.0",
-//     // optional 
-//     debug: true,
-//     protocol: "https",
-//     host: "api.github.com", // should be api.github.com for GitHub 
-//     pathPrefix: "", // for some GHEs; none for GitHub 
-//     timeout: 5000,
-//     headers: {
-//         // GitHub is happy with a unique user agent 
-//         "user-agent": "ONESADL" 
-//     }
-// });
 
 var repo_model = {
     /**
      * [search repos from github.com]
-     * @param  {[type]}   msg      query string
-     * @param  {Function} callback 
-     * @return {[type]}            [void]
+     * @param  {[type]}   condition      query string
+     * @return {[type]}   [void]
      */
-    search: async (msg, callback) => {
-        var msg = msg || {
-            q: 'bitcoin',
+    search: async (condition) => {
+        let cond = condition || {
+            q: 'blockchain',
             sort: 'forks',
             order: 'desc',
             per_page: 100
         }
-        return await octokit.search.repos(msg);
-        // octokit.search.repos(msg, function (err, data) {
-            // if (err) {return console.log(err)};
-            // return await treeData(data);
-            // callback(null, dataSet);
-            // callback(null, msg);
-        // });
-        //   octokit.search.repos(msg).then(function (err, data) {
-        //     if (err) { console.log(err)};
-            
-        //     // callback(null, dataSet);
-        //     callback(null, msg);
-        // });
+        return await octokit.search.repos(cond);
     },
 
-    treeData: function(dataSet) {
-    var languages = {};
-
-    var result = {
-        "name": "languages",
-        "children": []
-    }
-
-    if (dataSet && dataSet.items) {
-        var items = dataSet.items;
-
-        items.forEach(function(item, index) {
-            if (typeof languages[item.language] === "undefined") {
-                languages[item.language] = index;
-            };
-        })
-
-        for (var language in languages) {
-            if (language === "null") {
-                language = "others";
-            };
-
-            var root = {
-                "name": language,
-                "children": []
-            };
-
+    /**
+     * [format result of search repos]
+     * @param  {[type]}   data      
+     * @return {[type]}   [void]
+     */
+    format: function(data) {
+        var result = {
+            "name": "languages",
+            "children": []
+        }
+        var languages = {};
+        if (data && data.items) {
+            var items = data.items;
             items.forEach(function(item, index) {
-                var child = {
-                    "name": item.full_name,
-                    "watchers_count": item.watchers_count,
-                    "forks_count": item.forks_count
-                };
-
-                if (item.language === language || (item.language === "null" && language === "others")) {
-                    root.children.push(child);
+                if (typeof languages[item.language] === "undefined") {
+                    languages[item.language] = index;
                 };
             })
 
-            result.children.push(root);
-        }
-    }
-
-    return result;
-}
-}
-
-// from /public/js/utils.js
-function treeData(dataSet) {
-    var languages = {};
-
-    var result = {
-        "name": "languages",
-        "children": []
-    }
-
-    if (dataSet && dataSet.items) {
-        var items = dataSet.items;
-
-        items.forEach(function(item, index) {
-            if (typeof languages[item.language] === "undefined") {
-                languages[item.language] = index;
-            };
-        })
-
-        for (var language in languages) {
-            if (language === "null") {
-                language = "others";
-            };
-
-            var root = {
-                "name": language,
-                "children": []
-            };
-
-            items.forEach(function(item, index) {
-                var child = {
-                    "name": item.full_name,
-                    "watchers_count": item.watchers_count,
-                    "forks_count": item.forks_count
+            for (let language in languages) {
+                if (language === "null") {
+                    language = "others";
+                }
+                var root = {
+                    "name": language,
+                    "children": []
                 };
+                items.forEach(function (item, index) {
+                    var child = {
+                        "name": item.full_name,
+                        "forks_count": item.forks_count,
+                        "watchers_count": item.watchers_count
+                    };
 
-                if (item.language === language || (item.language === "null" && language === "others")) {
-                    root.children.push(child);
-                };
-            })
-
-            result.children.push(root);
+                    if (item.language === language || (item.language === "null" && language === "others")) {
+                        root.children.push(child);
+                    }
+                })
+                result.children.push(root);
+            }
         }
+        return result;
     }
-
-    return result;
 }
+
 
 module.exports = repo_model;
