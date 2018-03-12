d52fefdf2b1beb1dd2beba7152504eefa7d87be1
karst.xia <karst.xia@msn.cn>
2018-03-12 19:08:30 +0800

    add web page

---
+++ public/javascripts/repo/main.js
@@ -0,0 +1,37 @@
+(function() {
+    const SEARCH_API = 'http://127.0.0.1:8081/api/repo/search';
+    var loading = document.getElementById("onesadl");
+
+    Search.init(SEARCH_API);
+
+    Bar.init("TOP DEV LANGUAGES", "#barId");
+
+
+    function show(url) {
+        url = url || SEARCH_API;
+
+        var loader = setTimeout(function() {
+            loading.style.display = "block";
+        }, 300);
+
+        d3.json(url, function(err, result) {
+            if (err) {
+                clearTimeout(loader);
+                loading.style.display = "none";
+                alert("LOAD ERROR! CHECK NETWORK...")
+            };
+
+            const data = result.data;
+            Bar.show(Utils.getBarData(data));
+
+            clearTimeout(loader);
+            loading.style.display = "none";
+        });
+    }
+
+    Search.addListener(function(url) {
+        show(url);
+    })
+
+    show();
+})()
---
+++ public/javascripts/repo/search.js
@@ -0,0 +1,48 @@
+var Search = (function() {
+    var url = '',
+        apiUrl,
+        inputTxt,
+        searchBtn,
+        listeners = [],
+        query = {
+            q: 'onebitxy',
+            sort: "forks",
+            order: "desc",
+            per_page: 100
+        };
+
+    return {
+        init: function(url) {
+            apiUrl = url;
+
+            searchBtn = d3.select('#search-btn');
+            inputTxt = d3.select('#search-input');
+            searchBtn.on('click', function() {
+                var qStr = inputTxt.property('value');
+                var requestUrl = parseSearchUrl(qStr);
+                listeners.forEach(function(listener) {
+                    try {
+                        listener(url);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                })
+            })
+        },
+
+        addListener: function(listener) {
+            listeners.push(listener);
+        }
+    };
+
+    function parseSearchUrl(qStr) {
+        query.q = qStr || 'onebitxy';
+        
+        var arr = [];
+        for (var key in query) {
+            arr.push(key + '=' + query[key]);
+        };
+
+        return apiUrl + '?' + arr.join('&');
+    }
+}())
